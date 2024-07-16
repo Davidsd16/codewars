a406e4d02c21ca9047fcd397ab9618816c16043c
david <davidsanchezdiaz72@gmail.com>
2024-07-16 18:36:52 +0200

    Nueva kata realizada (Pilares)

--- src/nivel _8kyu/pilares.php
+++ src/nivel _8kyu/pilares.php
@@ -2,7 +2,16 @@
 
 function pillars($numberOfPillars, $dist, $width)
 {
-  // your code here
+    if ($numberOfPillars < 2) {
+        return 0; // No hay distancia si hay menos de 2 pilares
+    }
+    
+    $distCm = $dist * 100; // Convertir distancia a centímetros
+    $totalDistance = ($numberOfPillars - 1) * $distCm + ($numberOfPillars - 2) * $width;
+    
+    return $totalDistance;
 }
+
+echo pillars(4, 15, 30);
 
 ?>
